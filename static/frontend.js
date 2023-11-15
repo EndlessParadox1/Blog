c1480fe8d43304ef4d94aca5757262ad91ecfbbf
EndlessParadox1 <endlessparadox250@gmail.com>
2023-11-15 21:45:20 +0800

    Changes

--- static/frontend.js
+++ static/frontend.js
@@ -31,6 +31,7 @@ function index() {
                         content.append(tmp);
                     }
                     for (let arch of data.archs) {
+                        console.log(arch)
                         let tmp = `<li><a onclick="list(${arch})" class="link">${arch}</a></li>`;
                         archive.append(tmp);
                     }
